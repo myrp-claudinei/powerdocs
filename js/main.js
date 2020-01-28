55ab48f3526a2bb24c798fd3e298ecc861c9808e
Claudinei Ramiro <claudinei.ramiro@myrp.com.br>
2020-01-28 10:49:05 -0300

    Alterado comportamento do menu

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
-//$('#sidebar').hover(function () {
-    //$('#sidebar').removeClass('active');
-    //$('.sidebar-header h4').removeClass('invisible');
-   //$('#sidebar span').removeClass('invisible');
-//});
 $('#sidebar').hover(function () {
-    $(this).removeClass('active');
+    setTimeout(function () { 
+        $('#sidebar').removeClass('active');        
+    }, 100);
+
+    setTimeout(function () { 
+        $('.sidebar-header h4').removeClass('d-none');
+        $('#sidebar span').removeClass('d-none');
+        $('.suporte').removeClass('w-100');
+        $('.sobre').removeClass('w-100');
+        $('.suporte').addClass('w-50');
+        $('.sobre').addClass('w-50');
+    }, 400);
 });
 
 $('#sidebar').mouseleave(function () {
-    $('#sidebar').addClass('active');
-    $('.sidebar-header h4').addClass('invisible');
-    $('#sidebar span').addClass('invisible');
+    setTimeout(function () { 
+        $('#sidebar').addClass('active');
+    }, 400);
+
+    setTimeout(function () { 
+        $('.sidebar-header h4').addClass('d-none');
+        $('#sidebar span').addClass('d-none');   
+        $('.suporte').addClass('w-100');
+        $('.sobre').addClass('w-100');
+        $('.suporte').removeClass('w-50');
+        $('.sobre').removeClass('w-50');
+    }, 400);    
 });
 
 $('ul ul a').click( function(){
